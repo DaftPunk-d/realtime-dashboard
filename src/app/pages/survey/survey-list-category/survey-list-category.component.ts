81ecf9d7fd1f4725416aaf84c0be09d29dcd80b8
DaftPunk-d <mathiascleays@gmail.com>
2017-08-03 20:55:40 +0200

    validating answers

--- src/app/pages/survey/survey-list-category/survey-list-category.component.ts
+++ src/app/pages/survey/survey-list-category/survey-list-category.component.ts
@@ -16,14 +16,22 @@ export class SurveyListCategoryComponent implements OnInit {
   question: any;
 
   answers: any[];
+  isCorrect = false;
+  showValidation = false;
+  score: number = 0;
+
 
   chooseCategoryVisible = true;
   startQuiz = false;
+  startPage = false;
+  quizCompleted = false;
+
   loadingData = false;
   loadingCategories = false;
 
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {
+  }
 
   ngOnInit() {
     this.loadingData = false;
@@ -42,6 +50,7 @@ export class SurveyListCategoryComponent implements OnInit {
   }
 
   selectCategory(category) {
+    this.clearQuestionsAndAnswers();
     this.category = category;
     const categoryId = category.id;
     this.apiService.getQuestionsByCategory(categoryId)
@@ -62,13 +71,71 @@ export class SurveyListCategoryComponent implements OnInit {
       });
   }
 
-  clearQuestionsAndAnswers(){
-    this.questions.length = 0;
-    this.answers.length = 0;
+  clearQuestionsAndAnswers() {
+    if(this.questions && this.answers){
+      this.questions.length = 0;
+      this.answers.length = 0;
+    }
+    this.isCorrect = false;
+    this.score = 0;
   }
 
+  validateAnswer(answerToValidate, question) {
+    _.each(this.answers, (answer) => {
+      if (answerToValidate === answer) {
+        if (answer.isCorrect === 1) {
+          this.isCorrect = true;
+          this.score = this.score + 1;
+        } else {
+          this.isCorrect = false;
+        }
+        setTimeout(() => {
+          this.selectNextQuestion(question);
+        }, 1500)
+      }
+    })
+  }
 
+  selectNextQuestion(questionSelected: any){
+    const numberOfQuestionInArray = _.findIndex(this.questions, {'id': questionSelected.id});
+    if(numberOfQuestionInArray < this.questions.length - 1){
+      const newNumberOfQuestionInArray = numberOfQuestionInArray + 1;
+      this.question = this.questions[newNumberOfQuestionInArray];
+      this.showValidation = false;
+      this.getAnswerById(this.questions[newNumberOfQuestionInArray].id);
+    } else {
+      this.startQuiz = false;
+      this.quizCompleted = true;
+    }
+  }
 
+  getAnswerById(questionId: number){
+    this.apiService.getAnswersByQuestion(questionId).then((answers: any) => {
+      this.answers = answers;
+    })
+  }
+
+  retake(){
+    //fix the shuffle, only 3 questions available after retaking
+    this.questions = this.shuffle(this.questions);
+    this.chooseCategoryVisible = false;
+    this.startQuiz = false;
+    this.startPage = true;
+    this.quizCompleted = false;
+    this.score = 0;
+    this.isCorrect = false;
+    this.showValidation = false;
+  }
+
+  tryAnother(){
+    this.chooseCategoryVisible = true;
+    this.startQuiz = false;
+    this.startPage = true;
+    this.quizCompleted = false;
+    this.score = 0;
+    this.isCorrect = false;
+    this.showValidation = false;
+  }
 
 
   shuffle(array) {
